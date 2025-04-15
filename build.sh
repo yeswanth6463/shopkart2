0fb1c3fb289c2e51da5e0fd153f5f4b9122c4122
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-04-15 12:59:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 
+# Run the Django app with gunicorn
+gunicorn shopkart.wsgi:application --bind 0.0.0.0:8000
+
 if [[$CREATE_SUPERUSER]];
 then
 python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" fi
